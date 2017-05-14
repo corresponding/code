c7412e128e8126551d9e67edbbd7036e490e7e16
shengjie_mbp_2014 <ye5081@126.com>
2017-05-14 15:22:05 +0800

    新增插入排序算法

---
+++ data_structure_algorithm/chapter_02/example_02_18.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import random
+
+from utils import swap
+
+def insert_sort(list):
+    result = []
+    for i in range(len(list)):
+        insert(result, list[i])
+
+def insert(inserted_list, insert_data):
+    for i in reversed(range(len(inserted_list))):
+        if insert_data >inserted_list[i]:
+            inserted_list.insert(i, insert_data)
+
+if __name__ == '__main__':
+    list = random.sample(range(100), 30)
+    insert_sort(list)
+    print list
+
