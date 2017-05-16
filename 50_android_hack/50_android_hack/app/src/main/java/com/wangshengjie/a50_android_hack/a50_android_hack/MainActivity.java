46906b9cbadbf5f873afdd7efa206ece42ff2777
corresponding <ye5081@126.com>
2017-05-16 14:18:32 +0800

    新增50_android_hack工程

---
+++ 50_android_hack/50_android_hack/app/src/main/java/com/wangshengjie/a50_android_hack/a50_android_hack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wangshengjie.a50_android_hack.a50_android_hack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # data_structure_algorithm
 # 《数据结构、算法与应用--C++语言描述》的代码集，主要用python实现
+
+# 50_android_hack
+# 《打造高质量Android应用--Android开发必知的50个诀窍》
